c406ca32f98c2553c02494275dceafa305f8a025
Luke James <lukejjames11@gmail.com>
2026-07-30 13:13:01 -0400

    Scaffold Spring Boot project, configure Postgres connection

---
+++ src/main/java/com/setcelium/SetceliumApplication.java
@@ -0,0 +1,13 @@
+package com.setcelium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetceliumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SetceliumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/setcelium/SetceliumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.setcelium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SetceliumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
